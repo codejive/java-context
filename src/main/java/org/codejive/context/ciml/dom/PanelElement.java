e6b564ceb755fa885a9b6c36c6bcc5f1dcd9373a
Tako Schotanus <tako@codejive.org>
2024-02-20 23:12:09 +0100

    feat: first setup of CIML dom and layouter

---
+++ src/main/java/org/codejive/context/ciml/dom/PanelElement.java
@@ -0,0 +1,8 @@
+package org.codejive.context.ciml.dom;
+
+public class PanelElement extends ContextElement {
+
+    public PanelElement(ContextDocument ownerDocument) {
+        super(ownerDocument, "panel");
+    }
+}
---
+++ src/main/java/org/codejive/context/ciml/dom/ScreenElement.java
@@ -0,0 +1,8 @@
+package org.codejive.context.ciml.dom;
+
+public class ScreenElement extends ContextElement {
+
+    public ScreenElement(ContextDocument ownerDocument) {
+        super(ownerDocument, "screen");
+    }
+}
